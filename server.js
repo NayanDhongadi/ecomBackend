dfb972854217525f92d0243534e783d710a2d199
Nayan <nayandhongadi26@gmail.com>
2025-03-25 23:13:40 +0530

    change port

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use("/payments", paymentRoutes);
 
 app.get("/", (req, res) => res.send("E-commerce API Running"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT,()=>{
     console.log('app is running on --->',PORT)
